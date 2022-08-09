d2d5cb6188ef649ee0d8b1d7291de8099ab8b942
renandonizeti <renan.donizetics@gmail.com>
2022-08-09 19:19:55 -0300

    Preparando ambiente para desenvolvimento, primeiros arquivos

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const port = 3000
+
+app.get('/teste', (req, res) => res
+    .status(200)
+    .send({mensagem: 'Boas-vindos à API'
+}))
+
+app.listen(port, () => console.log(`Servidor está rodando na porta ${port}`))
+
+module.exports = app
